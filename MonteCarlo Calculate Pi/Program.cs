697e549caadcea7a2ccbdf208552e04c88f47246
David Tanderup <Davetanderup@gmail.com>
2019-04-01 10:34:55 -0400

    Add project files.

---
+++ MonteCarlo Calculate Pi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo_Calculate_Pi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
